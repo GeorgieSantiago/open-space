bf4184fcab85434fc7d4403da5b90933eae730f4
Georgie Santiago <georgie.santiago@MAC-402570.local>
2020-02-05 15:27:48 -0500

    get planets and stars

--- components/stellar-explorer/index.js
+++ components/stellar-explorer/index.js
@@ -4,12 +4,13 @@ import { Space } from './Space'
 import { FTL } from './components/FTL'
 
 export const SpaceExplorer = props => {
-    const [data] = useState({})
+    const { planets, star } = useSelector(store => store.explorer)
+
     const [isFtl, updateFtl] = useState(true)
     useEffect(() => {
         updateFtl(false)
     })
 
 
-    return (isFtl ? <Space /> : (<FTL />)
+    return (isFtl ? <Space star={star} planets={planets} /> : <FTL />)
 }
